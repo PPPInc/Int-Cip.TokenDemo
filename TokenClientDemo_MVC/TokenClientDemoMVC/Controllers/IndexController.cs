ea644acdb8dd2f45a714eb7f8f0d25ea316b2812
JohnS-CIP <johns@chargeitpro.com>
2014-12-29 10:13:34 -0700

    GB-1170 Separate simple MVC demo to repository.

---
+++ TokenClientDemo_MVC/TokenClientDemoMVC/Controllers/IndexController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CIP;
+using TokenClientDemoMVC.Models;
+
+namespace TokenClientDemoMVC.Controllers
+{
+    public class IndexController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Payment(Payment input)
+        {
+            /* Set your Private Key */
+            CIP.Token.ApiKey = "e5932e4dd41742cd81768c6ace7bedc9";
+
+            /* Create a Transaction */
+            var transaction = new CIP.Transaction()
+            {
+                Token = input.CipToken,
+                Amount = input.Amount,
+                TransactionType = input.TransactionType                
+            };
+
+            /* Process the transaction */
+            TransactionResult result = CIP.Token.RunTransaction(transaction);
+
+            /* Save the result to your database and/or render the result values to your receipt view */
+
+            var model = new Result()
+            {
+                AuthCode = result.AuthCode, 
+                AuthAmount = result.AuthAmount.ToString(), 
+                RefNumber = result.RefNum, 
+                Status = result.Status
+            };
+
+            return View("Receipt", model);
+        }
+
+
+    }
+}
---
+++ TokenClientDemo_MVC/TokenClientDemoMVC/Models/Result.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TokenClientDemoMVC.Models
+{
+    public class Result
+    {
+        public string AuthCode { get; set; }
+        public string AuthAmount { get; set; }
+        public string Status { get; set; }
+        public string RefNumber { get; set; }
+    }
+}
