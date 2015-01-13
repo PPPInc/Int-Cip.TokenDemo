33471d0ad0b141c019a7c526ce4c146e913c4b03
michaeljordan99 <michaeljordan99@hotmail.com>
2015-01-13 07:02:32 -0800

    Update payment.php
    Removed loader.  Updated url.

--- TokenDemo_PHP/payment.php
+++ TokenDemo_PHP/payment.php
@@ -58,7 +58,6 @@
 
                 <div class="form-group">
                     <button class="btn btn-default" type="submit">Submit</button>
-                    <img id="loader" src="ajax-loader-1.gif" style="margin: 0 0 0 10px;" class="hidden" />
                 </div>
 
             </div>
@@ -73,17 +72,7 @@
 	   	if($ciptoken != null) {
 
 	   		$response = httpRequest($ciptoken, $amount);
-
-	   		/* Why are AuthCode and AuthAmount returning 0's */
-
 	   		echo var_dump($response);
-
-			/*printf("<script>location.href='receipt.php?" . 
-				"Status=" . $response->{'Status'} . 
-				"&RefNum=" . $response->{'RefNum'} . 
-				"&AuthCode=" . $response->{'AuthCode'} . 
-				"&AuthAmount=" . $response->{'AuthAmount'} . 
-				"'</script>");*/
 	   	} 
 
 	   	function httpRequest($ciptoken, $amount) {
@@ -91,15 +80,14 @@
 			$data = array("token" => $ciptoken, "amount" => $amount, "transactionType" => "sale");                                                                    
 			$data_string = json_encode($data);                                                   
 			 
-			$ch = curl_init('http://cip-payment.azurewebsites.net/token/transaction.json');                                                                      
+			$ch = curl_init('https://psl.chargeitpro.com/token/transaction.json');                                                                      
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
 			curl_setopt($ch, CURLOPT_HTTPHEADER, array(	
-
 				'X-ApiKey: e5932e4dd41742cd81768c6ace7bedc9',                                                                           
 				'Content-Type: application/json', 
-			    'Content-Length: ' . strlen($data_string))                        
+			    	'Content-Length: ' . strlen($data_string))                        
 			);                                                                                                                   
 			$result = curl_exec($ch); 
 
@@ -120,7 +108,6 @@
             CIP.token.merchantName = 'Merchant1_23f1984001644e1ba7b4ca9506077e81';
 
             $('#payment-form').find('button').prop('disabled', false);
-            $('#loader').addClass('hidden');
 
             $('#payment-form').submit(function (event) {
 
@@ -128,7 +115,6 @@
 
                 // Disable the submit button and show ajax loader
                 $form.find('button').prop('disabled', true);
-                $('#loader').removeClass('hidden');
 
                 CIP.token.create($form, function (status, response) {
 
