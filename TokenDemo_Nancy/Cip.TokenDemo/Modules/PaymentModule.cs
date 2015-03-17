750de56443a244af91b5fe955e1bf858dd316737
michaeljordan99 <michaeljordan99@hotmail.com>
2015-03-17 10:55:37 -0700

    updates for accountdata

--- TokenDemo_Nancy/Cip.TokenDemo/Modules/PaymentModule.cs
+++ TokenDemo_Nancy/Cip.TokenDemo/Modules/PaymentModule.cs
@@ -26,6 +26,8 @@ namespace CIP.TokenDemo.Modules
 
                 /* This is the Server Side CIP integration */
                 CIP.Token.ApiKey = "e5932e4dd41742cd81768c6ace7bedc9";
+                CIP.Token.Url = "https://psl.chargeitpro.com/token/transaction.json";
+                //CIP.Token.Url = "http://localhost:57192/token/transaction.json";
                 
                 var transaction = new CIP.Transaction()
                 {
@@ -50,7 +52,7 @@ namespace CIP.TokenDemo.Modules
                 var payment = this.Bind<Payment>();
 
                 /* This is the Server Side CIP integration */
-                CIP.Token.ApiKey = "38646a19091049d59ea46e75847fe88f";
+                CIP.Token.ApiKey = "e5932e4dd41742cd81768c6ace7bedc9";
                 //CIP.Token.Url = "http://localhost:57192/token/transaction.json";
 
                 var transaction = new CIP.Transaction()
--- TokenDemo_Nancy/Cip.TokenDemo/Scripts/CIP.token.js
+++ TokenDemo_Nancy/Cip.TokenDemo/Scripts/CIP.token.js
@@ -4,7 +4,6 @@ CIP.token = new function () {
 
     var self = this;
     self.url = 'https://psl.chargeitpro.com/token/card.json';
-    //self.url = 'http://localhost:57192/token/card.json';
     self.merchantName = null;
     self.card = {};
 
@@ -70,7 +69,7 @@ function validate(request) {
     if (request.merchantName === undefined || request.merchantName === null) error = { 'message': 'Merchant Name required' };
 
     if (request.card === undefined || request.card === null
-        || request.card.number === undefined || request.card.number === null
+        || (request.card.number === undefined && request.card.accountdata === undefined)
         || request.card.expiration === undefined || request.card.expiration == null) error = { 'message': 'Card Number and Expiration Date required' };
 
     return error;
