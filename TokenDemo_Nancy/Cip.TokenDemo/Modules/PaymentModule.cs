c32698352f4d2123402f62aeca6540934ba84bdc
michaeljordan99 <michaeljordan99@hotmail.com>
2015-11-03 09:41:14 -0800

    ach payment demo

--- TokenDemo_Nancy/Cip.TokenDemo/Modules/PaymentModule.cs
+++ TokenDemo_Nancy/Cip.TokenDemo/Modules/PaymentModule.cs
@@ -162,6 +162,86 @@ namespace CIP.TokenDemo.Modules
                 return View["receipt", response.Result];
             };
 
+            /* ACH Payments*/
+
+            Get["/achpayment"] = parameters =>
+            {
+                var model = new Payment() { Environment = Environment };
+
+                return View["achpayment", model];
+            };
+
+            Post["/achpayment"] = parameters =>
+            {
+                var payment = this.Bind<Payment>();
+
+                /* This is the Server Side CIP integration */
+                CIP.Token.ApiKey = "e5932e4dd41742cd81768c6ace7bedc9";
+                //CIP.Token.ApiKey = "4d01abf522ce4025929b4515f3017f74";
+
+                //CIP.Token.Url = "https://psl.chargeitpro.com/token/transaction.json";
+                CIP.Token.Url = "http://localhost:57192/token/transaction.json";
+
+                var transaction = new CIP.Transaction()
+                {
+                    Token = payment.CipToken,
+                    ReferenceId = Guid.NewGuid().ToString(),
+                    Amount = payment.Amount,
+                    TransactionType = "ACHSale",
+                    Invoice = "123ABC",
+                    PONumber = "Test Po Number",
+                    OrderId = "Test OrderId",
+                    Description = "Test Description",
+                    Email = payment.Email,
+                    BillingAddress = new BillingAddress()
+                    {
+                        FirstName = payment.BillingFirstName,
+                        LastName = payment.BillingLastName,
+                        Company = payment.BillingCompany,
+                        Street = payment.BillingStreet,
+                        Street2 = payment.BillingStreet2,
+                        City = payment.BillingCity,
+                        State = payment.BillingState,
+                        Zip = payment.BillingZip,
+                        Country = payment.BillingCountry,
+                        Phone = payment.BillingPhone,
+                        Email = payment.BillingEmail
+                    },
+                    ShippingAddress = new ShippingAddress()
+                    {
+                        FirstName = payment.ShippingFirstName,
+                        LastName = payment.ShippingLastName,
+                        Company = payment.ShippingCompany,
+                        Street = payment.ShippingStreet,
+                        Street2 = payment.ShippingStreet2,
+                        City = payment.ShippingCity,
+                        State = payment.ShippingState,
+                        Zip = payment.ShippingZip,
+                        Country = payment.ShippingCountry,
+                        Phone = payment.ShippingPhone
+                    },
+                    Comments = payment.Comments
+                };
+
+                var response = CIP.Token.RunTransaction(transaction);
+
+                if (!response.Success)
+                {
+                    var model = new Payment() { Error = response.Error.Message };
+
+                    ViewBag.Referrer = "achpayment";
+
+                    return View["payment", model];
+                }
+                else
+                {
+                    ViewBag.Referrer = "payment";
+
+                    return View["receipt", response.Result];
+                }
+            };
+
+
             Get["/{param1}/{param2}"] = parameters =>
             {
                 return new RedirectResponse("/");
